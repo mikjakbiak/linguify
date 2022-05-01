d5e1a560fbe1c9690a0066d515420b0ad7a6c9d9
mathuwish <mathusan2008@live.ti>
2022-05-01 16:55:23 +0100

    database 1st push
    connection
    creating all tables
    and context data

---
+++ src/database/ConnectDB.java
@@ -0,0 +1,23 @@
+package database;
+
+import java.sql.*;
+
+public class ConnectDB 
+{
+        public static Connection getConnection() 
+    {
+        try 
+        {
+            Class.forName("org.sqlite.JDBC");
+            String url = "jdbc:sqlite:linguify.db"; // will create this database if it does not exist
+            Connection con = DriverManager.getConnection(url);
+                //JOptionPane.showMessageDialog(null, "Connection Established");
+            return con;
+        } 
+        catch (Exception e) 
+        {
+            //JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
