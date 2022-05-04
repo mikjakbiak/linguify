6b661ea4d72ebbe15f3d803a2c4cfcc7443ffd4c
ashw1739820 <w1739820@my.westminster.ac.uk>
2022-05-04 16:27:56 +0100

    updated final panels
    panels in views package

--- src/views/teacherPanel.java
+++ src/views/teacherPanel.java
@@ -8,12 +8,12 @@ package views;
  *
  * @author ash
  */
-public class teacherPanel extends javax.swing.JFrame {
+public class TeacherPanel extends javax.swing.JFrame {
 
     /**
      * Creates new form teacherPanel
      */
-    public teacherPanel() {
+    public TeacherPanel() {
         initComponents();
     }
 
@@ -146,20 +146,21 @@ public class teacherPanel extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(teacherPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TeacherPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(teacherPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TeacherPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(teacherPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TeacherPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(teacherPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TeacherPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new teacherPanel().setVisible(true);
+                new TeacherPanel().setVisible(true);
             }
         });
     }
